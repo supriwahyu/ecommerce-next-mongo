e76b7d37215f5522c8b3135b6c98322af6bf754f
supriwahyu <supriwahyu95@gmail.com>
2023-09-21 09:01:33 +0700

    preparation for upload images feature

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios"
 import { useRouter } from 'next/router';
 
-export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice }) {
+export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice, images }) {
     const [title, setTitle] = useState(existingTitle || '');
     const [description, setDescription] = useState(existingDescription || '');
     const [price, setPrice] = useState(existingPrice || '');
@@ -15,12 +15,25 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
         if (_id) {
             await axios.put('/api/products', { ...data, _id });
         } else {
-            await axios.post('/api/products', data);
+            await axios.post('/api/products', data, {
+                headers: { 'Content-Type': 'multipart/form-data' },
+            });
         }
         setGoToProducts(true);
     }
     if (goToProducts) {
         router.push('/products');
+    }
+    async function uploadImages(ev) {
+        const files = ev.target?.files;
+        if (files?.length > 0) {
+            const data = new FormData();
+            for (const file of files) {
+                data.append('file', file);
+            }
+            const res = await axios.post('/api/upload', data);
+            console.log(res.data);
+        }
     }
     return (
         <form onSubmit={saveProduct}>
@@ -28,6 +41,18 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
             <input type="text" placeholder="Product Name"
                 value={title}
                 onChange={ev => setTitle(ev.target.value)} />
+            <label>Photos</label>
+            <div className="mb-2">
+                <label className="w-24 h-24 border cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-grey-500 rounded-lg bg-gray-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>Upload</div>
+                    <input type="file" onChange={uploadImages} className="hidden" />
+                </label>
+                {!images?.length && (
+                    <div>No photos in this product</div>
+                )}</div>
             <label>Description</label>
             <textarea placeholder="description"
                 value={description}
