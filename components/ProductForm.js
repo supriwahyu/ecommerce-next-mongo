1a94363df3333a3aca2f5f548fbcc3a15acf9ed5
supriwahyu <supriwahyu95@gmail.com>
2023-09-22 23:24:09 +0700

    uploading is working now

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios"
 import { useRouter } from 'next/router';
+import Spinner from "./spinner";
 
 export default function ProductForm({ _id, title: existingTitle, description: existingDescription, price: existingPrice, images: existingImages }) {
     const [title, setTitle] = useState(existingTitle || '');
@@ -8,17 +9,16 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
     const [price, setPrice] = useState(existingPrice || '');
     const [images, setImages] = useState(existingImages || []);
     const [goToProducts, setGoToProducts] = useState(false);
+    const [isUploading, setIsUploading] = useState(false);
     const router = useRouter();
 
     async function saveProduct(ev) {
         ev.preventDefault();
-        const data = { title, description, price };
+        const data = { title, description, price, images };
         if (_id) {
             await axios.put('/api/products', { ...data, _id });
         } else {
-            await axios.post('/api/products', data, {
-                headers: { 'Content-Type': 'multipart/form-data' },
-            });
+            await axios.post('/api/products', data);
         }
         setGoToProducts(true);
     }
@@ -28,6 +28,7 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
     async function uploadImages(ev) {
         const files = ev.target?.files;
         if (files?.length > 0) {
+            setIsUploading(true);
             const data = new FormData();
             for (const file of files) {
                 data.append('file', file);
@@ -39,6 +40,7 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                 return [...oldImages, ...res.data.result.links];
                 // }
             });
+            setIsUploading(false);
         }
     }
     return (
@@ -48,12 +50,17 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                 value={title}
                 onChange={ev => setTitle(ev.target.value)} />
             <label>Photos</label>
-            <div className="mb-2">
+            <div className="mb-2 flex flex-warp gap-1">
                 {!!images?.length && images.map(links => (
-                    <div key={links}>
-                        <img src={links} alt="" />
+                    <div key={links} className="h-24">
+                        <img src={links} className="rounded-lg" alt="" />
                     </div>
                 ))}
+                {isUploading && (
+                    <div className="h-24 flex items-center">
+                        <Spinner />
+                    </div>
+                )}
                 <label className="w-24 h-24 border cursor-pointer text-center flex items-center justify-center text-sm gap-1 text-grey-500 rounded-lg bg-gray-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -61,9 +68,7 @@ export default function ProductForm({ _id, title: existingTitle, description: ex
                     <div>Upload</div>
                     <input type="file" onChange={uploadImages} className="hidden" />
                 </label>
-                {!images?.length && (
-                    <div>No photos in this product</div>
-                )}</div>
+            </div>
             <label>Description</label>
             <textarea placeholder="description"
                 value={description}
